654250d5a0e7680a1a249f34eb6920a0a957891e
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-23 02:00:02 +0900

    rails gで自動生成しない設定を追加(CSS, JavaScript, Helper)

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,10 @@ module DevExam
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
